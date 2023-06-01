28e97597c4dea0555ff8500b91943eafd47df6bf
hasinmj <70891661+hasinmj@users.noreply.github.com>
2023-06-01 16:09:10 +0600

    Add files via upload

---
+++ imagetotext.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+print(pytesseract.image_to_string(Image.open('audrey.png'), lang='eng'))
